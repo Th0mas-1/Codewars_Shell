4f50f268497bb940e8f335f82d70b88c06021d33
ThomasW <tom.0384@gmail.com>
2021-06-02 17:09:57 +0200

    ExpressionsMatter2 optimized

--- ExpressionsMatter2.sh
+++ ExpressionsMatter2.sh
@@ -1,7 +1,5 @@
-#!/bin/zsh
-
 a=$1
 b=$2
 c=$3
 
-echo $(( a + b + c )) "\n"$(( a + b * c )) "\n"$(( (a + b) * c )) "\n"$(( a + (b * c) )) "\n"$(( a * b + c )) "\n"$(( (a * b) + c )) "\n"$(( a * (b + c) )) "\n"$(( a * b * c )) | sort --numeric-sort --reverse | head --lines=1 
+echo -e $((a+b+c)) '\n'$((a+b*c)) '\n'$(((a+b)*c)) '\n'$((a+(b*c))) '\n'$((a*b+c)) '\n'$(((a*b)+c)) '\n'$((a*(b+c))) '\n'$((a*b*c)) | sort -n | tail -1
